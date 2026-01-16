b33af0db746a57ab337589d5a7d059b98daa5899
Adam Mustapha <matawalle4u@gmail.com>
2026-01-16 01:41:38 +0100

    Configured app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MemoModule } from './memo/memo.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [MemoModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+
+    TypeOrmModule.forRoot({
+      type: 'postgres', // or 'mysql', 'sqlite', etc.
+      host: 'localhost',
+      port: 5432,
+      username: 'your_username',
+      password: 'your_password',
+      database: 'memo_db',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true, // Set to false in production
+    }),
+    MemoModule
+  ],
 })
 export class AppModule {}
